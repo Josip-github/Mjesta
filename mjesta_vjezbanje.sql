54734aa7d53ec75220641926823d09004086c588
Josip-github <josipcota1@gmail.com>
2021-05-18 07:31:04 +0200

    Dubrovačko-neretvanska županija
    Koliko ima mjesta u Dubrovačko-neretvanskoj županiji?

--- mjesta_vjezbanje.sql
+++ mjesta_vjezbanje.sql
@@ -6,3 +6,13 @@ select * from mjesto where postanskibroj='10310';
 
 #ili drugi način:
 select * from mjesto where naziv='Ivanić-Grad';
+
+#Koliko ima mjesta u Dubrovačko-neretvanskoj županiji?
+select * from mjesto where naziv='Dubrovnik';
+
+#Dubrovačko-neretvanska županija ima int vrijednost = 19
+select * from mjesto where zupanija=19;
+#Odgovor: 56 je mjesta u Dubrovačko-neretvanskoj županiji
+
+#ili:
+select count(*) from mjesto where zupanija=19;
