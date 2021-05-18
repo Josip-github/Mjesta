a4b19c284b2d710b25c2a58473ed9b1f26d877b3
Josip-github <josipcota1@gmail.com>
2021-05-18 07:26:02 +0200

    Ivanić-Grad
    Kojoj županiji pripada Ivanić Grad?

--- mjesta_vjezbanje.sql
+++ mjesta_vjezbanje.sql
@@ -1,2 +1,8 @@
 # koliko ima redaka u tablici mjesto?
 select count(*) from mjesto;
+
+#Pronađi kojoj županiji pripada Ivanić Grad
+select * from mjesto where postanskibroj='10310';
+
+#ili drugi način:
+select * from mjesto where naziv='Ivanić-Grad';
